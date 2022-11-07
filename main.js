e6141b07071ca0e14582e29fa7a309b85df79f5b
unknown <valentinechima95@gmail.com>
2022-11-07 22:08:38 +0100

    update on form

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ function myFunction(x) {
 
 
 
+
+const menu_btn = document.querySelector(".hamburger-menu");
+const mobile_menu = document.querySelector(".mobile-nav");
+
+menu_btn.addEventListener('click', function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
+
 // to initialize on-scroll animations
 
 new WOW().init();
--- real.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $fullname = $_POST['fullname'];
-    $email = $_POST['email'];
-    $textarea = $_POST['textarea'];
-
-    // Database connection
-?>
