6b002b33a6be4105f41ba10ebca0bca3991f6b06
unknown <valentinechima95@gmail.com>
2022-11-17 22:02:07 +0100

    scroll reveal js

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ menu_btn.onclick = function(){
     mobile_menu.classList.toggle('is-active');
 }
 
-// to initialize on-scroll animations
+// scroll reveal
 
-new WOW().init();
+ScrollReveal({
+    reset: true,
+    distance:'60px',
+    delay: 400,
+    duration: 2500
+});
+
+ScrollReveal().reveal('.about-me, .connect-box1', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.my-photo, .connect-box2', { delay: 600, origin: 'right', interval: 200 });
+ScrollReveal().reveal(' h1, .portfolio-set, .portfolio-box-container', { delay: 500, origin: 'top', interval: 200 });
+ScrollReveal().reveal('.social-connect a', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.service-box, .tool-box, .exp-box, .footer', { delay: 500, origin: 'bottom', interval: 200 });
